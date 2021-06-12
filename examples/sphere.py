537928f8b344f4a0c36ade55e85a745c8ec27046
songruixin <songzhongyang@mbp15product.local>
2021-06-12 11:15:13 -0230

    finish sphere example

--- examples/sphere.py
+++ examples/sphere.py
@@ -1,8 +1,31 @@
 from capytaine import *
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 
-# Adding a new floating body
+# Loading a mesh: here is a simple geometry
 sphere = Sphere(radius=1.0, center=(0, 0, -2), name="my_sphere")
 sphere.show()
+
+# Defining dof (degree of freedom)
+# in the x-direction
+sphere.dofs['Surge'] = [(1, 0, 0) for face in sphere.mesh.faces]
+sphere.add_translation_dof(name="Heave")
+
+# Defining linear potential flow problem
+from numpy import infty
+problem = RadiationProblem(body=sphere, radiating_dof="Heave", omega=1.0, sea_bottom=-infty, g=9.81, rho=1000)
+
+# Solving the problem
+solver = BEMSolver()
+result = solver.solve(problem)
+
+# The other problem
+problem2 = RadiationProblem(body=sphere, radiating_dof="Surge", omega=1.0)
+result2 = solver.solve(problem2)
+
+# Gathering the results
+dataset = assemble_dataset([result, result2])
+
+dataset['added_mass'].sel(radiating_dof=["Surge", "Heave"], influenced_dof=["Surge", "Heave"], omega=1.0)
+print(dataset)
