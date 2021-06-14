f18ad727da677e74f58bf62ebbcb7a11a6b9557e
tsuzzy <songzhongyang@mbp15product.local>
2021-06-14 09:21:20 -0230

    1st computation: added calculation varaible frequency

--- gravity_density.py
+++ gravity_density.py
@@ -6,44 +6,42 @@
 
 import capytaine as cpt
 import capytaine.io.xarray as xarr
-import numpy as np
-import math
 
 import logging
 logging.basicConfig(level=logging.INFO, format="%(levelname)s:\t%(message)s")
 
-# # Loading a mesh: sphere
-# sphere = cpt.Sphere(radius=0.5, center=(0, 0, -2), name="MySphere")
 
-# sphere = cpt.FloatingBody.from_file("examples/boat_200.mar", file_format="mar", name="unit")
-
-
-
+import math
+import numpy as np
 # Profile of the axisymmetric body
 def shape(z):
-    return 0.1*(math.pow(-(z+1), 2) + 16)
+    return 0.1*(-math.pow((z+1), 2) + 16)
 
 # Generate the mesh and display it with VTK.
 geometry = cpt.FloatingBody(
     cpt.AxialSymmetricMesh.from_profile(shape, z_range=np.linspace(-5, 0, 30), nphi=40)
 )
 
-geometry.show()
-
-
 # Adding degree of freedom (DOF)
 geometry.add_translation_dof(name="Heave")
 
+
 # Set up the problems
-bottoms = np.linspace(-5,-100,50)
-# planets = ['Earth', 'Moon', 'Mars']
+# Problem set#1: RadiationProblem related to water depth
+bottoms = np.linspace(-5,-50,50)
 problems_seabottom = [cpt.RadiationProblem(body=geometry, radiating_dof='Heave', sea_bottom=bottom)
             for bottom in bottoms]
 
-# Problems relating to liquid density
+# Problem set#2: RadiationProblem related to liquid density
 rhos = np.linspace(0.5, 13.5, 52)
 problems_density = [cpt.RadiationProblem(body=geometry, radiating_dof='Heave', rho=r)
             for r in rhos]
+
+# Problem set#3: RadiationProblem related to frequency omega
+omegas = np.linspace(0.1, 5.0, 50)
+problems_freq = [cpt.RadiationProblem(body=geometry, radiating_dof='Heave', omega=om)
+            for om in omegas]
+
 
 # Solve the problems using the axial symmetry
 solver = cpt.BEMSolver(engine=cpt.HierarchicalToeplitzMatrixEngine())
@@ -54,11 +52,14 @@ dataset1 = xarr.assemble_dataset(results_seabottom, wavelength=True, wavenumber=
 results_density = [solver.solve(problem) for problem in sorted(problems_density)]
 dataset2 = xarr.assemble_dataset(results_density, wavelength=True)
 
+results_freq = [solver.solve(problem) for problem in sorted(problems_freq)]
+dataset3 = xarr.assemble_dataset(results_freq, wavelength=True)
+
 
 # Plot results
 import matplotlib.pyplot as plt
 
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(1,3)
 fig.suptitle('Variable controls')
 
 axs[0].plot(bottoms, dataset1['wavelength'], label="Wavelength")
@@ -70,10 +71,10 @@ axs[0].plot(
     bottoms,
     dataset1['radiation_damping'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Radiation damping")
+axs[0].set(xlabel='water depth')
 axs[0].grid()
 axs[0].legend()
 
-# axs[1].plot(rhos, dataset2['wavelength'], label="Wavelength")
 axs[1].plot(
     rhos,
     dataset2['added_mass'].sel(radiating_dof='Heave',influenced_dof='Heave'),
@@ -82,7 +83,21 @@ axs[1].plot(
     rhos,
     dataset2['radiation_damping'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Radiation damping")
+axs[1].set(xlabel='liquid density')
 axs[1].grid()
 axs[1].legend()
+
+axs[2].plot(omegas, dataset3['wavelength'], label="Wavelength")
+axs[2].plot(
+    omegas,
+    dataset3['added_mass'].sel(radiating_dof='Heave',influenced_dof='Heave'),
+    label="Added mass")
+axs[2].plot(
+    omegas,
+    dataset3['radiation_damping'].sel(radiating_dof='Heave',influenced_dof='Heave'),
+    label="Radiation damping")
+axs[2].set(xlabel='frequency (rad/s)')
+axs[2].grid()
+axs[2].legend()
 
 plt.show()
