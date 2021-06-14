ab0f293f11424f027a6c1089e43193840ce628b9
tsuzzy <tsuzsrx@gmail.com>
2021-06-14 11:05:28 -0230

    1st computation: added variable -- gravity

--- gravity_density.py
+++ gravity_density.py
@@ -42,6 +42,10 @@ omegas = np.linspace(0.1, 5.0, 50)
 problems_freq = [cpt.RadiationProblem(body=geometry, radiating_dof='Heave', omega=om)
             for om in omegas]
 
+# Problem set#4: RadiationProblem related to acceleration of gravity
+gravities = np.linspace(1, 20, 50)
+problems_grav = [cpt.RadiationProblem(body=geometry, radiating_dof='Heave', g=grav)
+            for grav in gravities]
 
 # Solve the problems using the axial symmetry
 solver = cpt.BEMSolver(engine=cpt.HierarchicalToeplitzMatrixEngine())
@@ -55,49 +59,64 @@ dataset2 = xarr.assemble_dataset(results_density, wavelength=True)
 results_freq = [solver.solve(problem) for problem in sorted(problems_freq)]
 dataset3 = xarr.assemble_dataset(results_freq, wavelength=True)
 
+results_grav = [solver.solve(problem) for problem in sorted(problems_grav)]
+dataset4 = xarr.assemble_dataset(results_grav, wavelength=True)
+
 
 # Plot results
 import matplotlib.pyplot as plt
 
-fig, axs = plt.subplots(1,3)
+fig, axs = plt.subplots(2,2)
 fig.suptitle('Variable controls')
 
-axs[0].plot(bottoms, dataset1['wavelength'], label="Wavelength")
-axs[0].plot(
+axs[0,0].plot(bottoms, dataset1['wavelength'], label="Wavelength")
+axs[0,0].plot(
     bottoms,
     dataset1['added_mass'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Added mass")
-axs[0].plot(
+axs[0,0].plot(
     bottoms,
     dataset1['radiation_damping'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Radiation damping")
-axs[0].set(xlabel='water depth')
-axs[0].grid()
-axs[0].legend()
+axs[0,0].set(xlabel='water depth')
+axs[0,0].grid()
+axs[0,0].legend()
 
-axs[1].plot(
+axs[0,1].plot(
     rhos,
     dataset2['added_mass'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Added mass")
-axs[1].plot(
+axs[0,1].plot(
     rhos,
     dataset2['radiation_damping'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Radiation damping")
-axs[1].set(xlabel='liquid density')
-axs[1].grid()
-axs[1].legend()
+axs[0,1].set(xlabel='liquid density')
+axs[0,1].grid()
+axs[0,1].legend()
 
-axs[2].plot(omegas, dataset3['wavelength'], label="Wavelength")
-axs[2].plot(
+axs[1,0].plot(omegas, dataset3['wavelength'], label="Wavelength")
+axs[1,0].plot(
     omegas,
     dataset3['added_mass'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Added mass")
-axs[2].plot(
+axs[1,0].plot(
     omegas,
     dataset3['radiation_damping'].sel(radiating_dof='Heave',influenced_dof='Heave'),
     label="Radiation damping")
-axs[2].set(xlabel='frequency (rad/s)')
-axs[2].grid()
-axs[2].legend()
+axs[1,0].set(xlabel='frequency (rad/s)')
+axs[1,0].grid()
+axs[1,0].legend()
+
+axs[1,1].plot(
+    gravities,
+    dataset4['added_mass'].sel(radiating_dof='Heave',influenced_dof='Heave'),
+    label="Added mass")
+axs[1,1].plot(
+    gravities,
+    dataset4['radiation_damping'].sel(radiating_dof='Heave',influenced_dof='Heave'),
+    label="Radiation damping")
+axs[1,1].set(xlabel='acceleration of gravity')
+axs[1,1].grid()
+axs[1,1].legend()
 
 plt.show()
